dd5ef6942bfdde47696e430015372614fa1bdbda
Simchaja <53301183+Simchaja@users.noreply.github.com>
2020-10-09 19:54:44 +0200

    Start docentenvak
    Docentenvak.php en docenten.css aangemaakt. En gestart met het maken van de vierkantjes.

---
+++ !Test/docentenvak.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="Docenten.css">
+	</head>
+	<body>
+		<h1>Docenten</h1>
+		<div class = "docentenvakken">
+			<div class = "docentenvak" id="1">
+		
+				<div class = "green">
+			
+				</div>
+				<div class = "white">
+			
+				</div>
+			</div>
+			<div class = "docentenvak" id="2">
+		
+				<div class = "green">
+			
+				</div>
+				<div class = "white">
+			
+				</div>
+			</div>
+		</div>
+	</body>
+</html>

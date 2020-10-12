96e2453e9b8a8a2a0d11cab3c8763538deeb008a
Feike Falkena <feike@feikefalkena.nl>
2020-10-12 20:23:01 +0200

    Login.php pagina ipv in map
    /Login/index.php verplaatst naar login.php

--- pages/Login/LoginHandler.php
+++ pages/Login/LoginHandler.php
@@ -26,10 +26,10 @@ if ($result->num_rows > 0) {
     setcookie("auth", auth(128), time()+3600, '/; samesite=strict');
     setcookie("domain", $domain, time()+3600, '/; samesite=strict');
     
-    header("location:pagina.php");
+    header("location:/../nieuws.php");
   }
 } else { //if password and/or username are incorrect, send to login page
-    header("location:index.php");
+    header("location:../login.php");
 }
 $conn->close(); //Close connection
 

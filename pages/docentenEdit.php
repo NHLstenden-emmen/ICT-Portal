be73c85ef23ef5d720db98bb0c1cc831ec86968c
RemcoHas <rhassing@outlook.com>
2020-10-19 21:37:23 +0200

    Fix

--- pages/docentenEdit.php
+++ pages/docentenEdit.php
@@ -8,71 +8,68 @@
  
 if($result->num_rows > 0){
     if(isset($_POST['submitAction'])){
-    if(!empty($_POST['docentVoornaam'])){
-        if(!empty($_POST['docentAchternaam'])){
-            if(!empty($_POST['docentEmail'])){
-                if(!empty($_POST['docentGebruikersnaam'])){
-                    if(!empty($_POST['docentWachtwoord'])){
-                        if($_POST['docentWachtwoord'] == $_POST['docentWachtwoordHerhaal']){
-                            
-                            $docentWachtwoord = md5($_POST['docentWachtwoordHerhaal']);
+        if(!empty($_POST['docentVoornaam'])){
+            if(!empty($_POST['docentAchternaam'])){
+                if(!empty($_POST['docentEmail'])){
+                    if(!empty($_POST['docentGebruikersnaam'])){
+                        if(!empty($_POST['docentWachtwoord'])){
+                            if($_POST['docentWachtwoord'] == $_POST['docentWachtwoordHerhaal']){
+                                
+                                $docentWachtwoord = md5($_POST['docentWachtwoordHerhaal']);
 
-                    
-                            if(!empty($_FILES["docentFoto"]["name"])) { 
-                                $fileName = basename($_FILES["docentFoto"]["name"]); 
-                                
-                                $fileType = pathinfo($fileName, PATHINFO_EXTENSION); 
-                                
-                                $allowTypes = array('jpg','png','jpeg','gif'); 
-                                if(in_array($fileType, $allowTypes)){ 
-                                    $image = $_FILES['docentFoto']['tmp_name']; 
-                                    $imgContent = addslashes(file_get_contents($image)); 
-                                }
-                            } else {
-                                $imgContent = $docentData['foto'];
+                        
+                                if(!empty($_FILES["docentFoto"]["name"])) { 
+                                    $fileName = basename($_FILES["docentFoto"]["name"]); 
+                                    
+                                    $fileType = pathinfo($fileName, PATHINFO_EXTENSION); 
+                                    
+                                    $allowTypes = array('jpg','png','jpeg','gif'); 
+                                    if(in_array($fileType, $allowTypes)){ 
+                                        $image = $_FILES['docentFoto']['tmp_name']; 
+                                        $imgContent = addslashes(file_get_contents($image)); 
+                                        $DB->Get("UPDATE docenten SET foto = '{$imgContent}' WHERE docent_id = '{$docentID}'");
+                                    }
+                                } 
+                                    
+
+                                $updateStatement = $DB->Get("UPDATE docenten SET 
+                                    voornaam = '{$_POST["docentVoornaam"]}', 
+                                    achternaam = '{$_POST["docentAchternaam"]}', 
+                                    email = '{$_POST["docentEmail"]}',
+                                    telefoonnummer = '{$_POST["docentTelefoonnummer"]}',
+                                    gebruikersnaam = '{$_POST["docentGebruikersnaam"]}',
+                                    wachtwoord = '{$docentWachtwoord}',
+                                    twitter = '{$_POST["docentTwitter"]}',
+                                    linkedin = '{$_POST["docentLinkedin"]}',
+                                    instagram = '{$_POST["docentInstagram"]}'
+                                        WHERE docent_id = '{$docentID}'");
+
+                                    header('Location: /docentenEdit');
+
                             }
-                                
-
-
-                            $updateStatement = $DB->Get("UPDATE docenten SET 
-                                voornaam = '{$_POST["docentVoornaam"]}', 
-                                achternaam = '{$_POST["docentAchternaam"]}', 
-                                email = '{$_POST["docentEmail"]}',
-                                telefoonnummer = '{$_POST["docentTelefoonnummer"]}',
-                                gebruikersnaam = '{$_POST["docentGebruikersnaam"]}',
-                                wachtwoord = '{$docentWachtwoord}',
-                                foto = '{$imgContent}',
-                                twitter = '{$_POST["docentTwitter"]}',
-                                linkedin = '{$_POST["docentLinkedin"]}',
-                                instagram = '{$_POST["docentInstagram"]}'
-                                    WHERE docent_id = '{$docentID}'");
-
-                            header('Location: /docentenEdit');
-
+                            else {
+                                echo "Wachtwoorden niet gelijk";
+                            }
                         }
                         else {
-                            echo "Wachtwoorden niet gelijk";
+                            echo "Geen Wachtwoord ingevoerd.";
                         }
                     }
                     else {
-                        echo "Geen Wachtwoord ingevoerd.";
+                        echo "Geen Gebruikersnaam ingevoerd.";
                     }
                 }
                 else {
-                    echo "Geen Gebruikersnaam ingevoerd.";
+                    echo "Geen Email ingevoerd.";
                 }
             }
             else {
-                echo "Geen Email ingevoerd.";
+                echo "Geen achternaam ingevoerd.";
             }
         }
         else {
-            echo "Geen achternaam ingevoerd.";
-        }
-    }
-    else {
-        echo "Geen voornaam ingevoerd.";
-    }   
+            echo "Geen voornaam ingevoerd.";
+        }   
     }
 }
 ?>
@@ -89,7 +86,7 @@ if($result->num_rows > 0){
         }
         ?>
         <input type="file" name="docentFoto" style="width: 65%;"><br />
-
+        <br />
         <div class="subTitle">Persoonlijke informatie</div><br />
         <label for="docentVoornaam">Voornaam*</label><br />
         <input type="text" value="<?= $docentData['voornaam'] ?>" name="docentVoornaam" placeholder="Voornaam" style="width: 65%;" required><br />
@@ -105,8 +102,8 @@ if($result->num_rows > 0){
         
         <label for="docentGebruikersnaam">Gebruikersnaam*</label><br />
         <input type="text" value="<?= $docentData['gebruikersnaam'] ?>" name="docentGebruikersnaam" placeholder="Gebruikersnaam" style="width: 65%;" required><br />
-
-        <div class="subTitle">Socials</div>
+        <br />
+        <div class="subTitle">Socials</div><br />
         <label for="docenttwitter">Twitter</label><br />
         <input type="text" value="<?= $docentData['twitter'] ?>" name="docentTwitter" placeholder="Twitter" style="width: 65%;"><br />
         
@@ -115,7 +112,7 @@ if($result->num_rows > 0){
         
         <label for="docentInstagram">Instagram</label><br />
         <input type="text" value="<?= $docentData['instagram'] ?>" name="docentInstagram" placeholder="Instagram" style="width: 65%;"><br />
-
+        <br />
         <div class="subTitle">Beveiliging</div><br />
         <label for="docentWachtwoord">Wachtwoord*</label><br />
         <input type="password" name="docentWachtwoord" placeholder="Wachtwoord" style="width: 65%;" required><br />

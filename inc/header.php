9a8816cacfceca632fea406e21167d626df0ec76
kevinsmul <smulders.kevin.1999@gmail.com>
2020-10-19 22:13:35 +0200

    fixed the merg conflicts

--- inc/header.php
+++ inc/header.php
@@ -15,8 +15,8 @@
 
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet">
 
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<?php
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <?php
             if(!isset($_GET['page']) || $_GET['page'] == ''){
                 $page = 'nieuws'; //If no page specified
             } else {
@@ -56,42 +56,3 @@
         ?>
     </head>
     <body>
-<nav class="navbar">
-		<div class="logo">
-            <a href="/index" class="logo"><img src="css/images/default_logo_white.png" alt="ICT Portal"></a>
-		</div>
-		<div class="navbar_items">
-			<ul>
-                <li class="<?= ($activePage == 'nieuws') ? 'active':''; ?>" onclick="window.location.href='/nieuws'">Nieuws</li>
-                <li class="<?= ($activePage == 'vakken') ? 'active':''; ?>" onclick="window.location.href='/vakken'">Vakken</li>
-                <li class="<?= ($activePage == 'docenten') ? 'active':''; ?>" onclick="window.location.href='/docenten'">Docenten</li>
-                <li class="<?= ($activePage == 'contact') ? 'active':''; ?>" onclick="window.location.href='/contact'">Contact</li>
-                <li class="<?= ($activePage == 'login') ? 'active':''; ?>" onclick="window.location.href='/login'">Login</li>
-                <div class="divider"></div>
-                <div class="darkmodeSwitch"><i class="fa fa-adjust fa-lg fa-fw" aria-hidden="true"></i></div>
-
-			</ul>
-		</div>
-    </nav>
-    
-	<div class="banner_image">
-		<div class="banner_content-left">
-			<div><?= isset($activePage) ? $activePage : "ICT Portal" ?><br/></div>
-                <span>Laatst bijgewerkt om 15:00</span><br />
-                <div class="inputSearchField">
-                    <form>  
-                        <div class="inputWithIcon">
-                            <input type="text" placeholder="Zoek een trefwoord">
-                            <i class="fa fa-search fa-lg fa-fw" aria-hidden="true"></i>
-                        </div>
-                    </form>
-                </div>
-            </div>
-            <div class="banner_content-right">
-			<div>Vakkenblok.<br/>
-				<span>Span Vakkenblok.</span>
-			</div>
-		</div>
-        </div>
-
-	</div>
--- inc/nav.php
+++ inc/nav.php
@@ -1,8 +1,39 @@
-<?php $activePage = basename($_SERVER['REQUEST_URI'], ".php");?>
-<ul class="nav navbar-nav">
-      <li class="<?= ($activePage == 'nieuws') ? 'active':''; ?>"><a href="nieuws">nieuws</a></li>
-      <li class="<?= ($activePage == 'vakken') ? 'active':''; ?>"><a href="vakken">Vakken</a></li>
-      <li class="<?= ($activePage == 'docenten') ? 'active':''; ?>"><a href="docenten">docenten</a></li>
-      <li class="<?= ($activePage == 'login') ? 'active':''; ?>"><a href="login">Login</a></li>
-      
-</ul>
+<nav class="navbar">
+      <div class="logo">
+      <a href="/index" class="logo"><img src="css/images/default_logo_white.png" alt="ICT Portal"></a>
+      </div>
+      <div class="navbar_items">
+            <ul>
+            <li class="<?= ($activePage == 'nieuws') ? 'active':''; ?>" onclick="window.location.href='nieuws'">Nieuws</li>
+            <li class="<?= ($activePage == 'vakken') ? 'active':''; ?>" onclick="window.location.href='vakken'">Vakken</li>
+            <li class="<?= ($activePage == 'docenten') ? 'active':''; ?>" onclick="window.location.href='docenten'">Docenten</li>
+            <li class="<?= ($activePage == 'contact') ? 'active':''; ?>" onclick="window.location.href='contact'">Contact</li>
+            <li class="<?= ($activePage == 'login') ? 'active':''; ?>" onclick="window.location.href='login'">Login</li>
+            <div class="divider"></div>
+            <div class="darkmodeSwitch"><i class="fa fa-adjust fa-lg fa-fw" aria-hidden="true"></i></div>
+
+            </ul>
+      </div>
+</nav>
+
+<div class="banner_image">
+      <div class="banner_content-left">
+            <div><?= isset($activePage) ? $activePage : "ICT Portal" ?><br/></div>
+            <span>Laatst bijgewerkt om 15:00</span><br />
+            <div class="inputSearchField">
+                  <form>  
+                  <div class="inputWithIcon">
+                        <input type="text" placeholder="Zoek een trefwoord">
+                        <i class="fa fa-search fa-lg fa-fw" aria-hidden="true"></i>
+                  </div>
+                  </form>
+            </div>
+      </div>
+      <div class="banner_content-right">
+            <div>Vakkenblok.<br/>
+                  <span>Span Vakkenblok.</span>
+            </div>
+      </div>
+      </div>
+
+</div>

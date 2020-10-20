93673c2874f55836cfb08d69832b095dfd4624a2
Feike Falkena <feike@feikefalkena.nl>
2020-10-21 00:34:53 +0200

    Update sidebar
    Webshop link toegevoegd.

--- inc/sidebar.php
+++ inc/sidebar.php
@@ -104,7 +104,7 @@
     <div class="row3 row">
         <div class="rowContent">
             <h3>Webshop</h3><br>
-            <a href="#"><div class="shop"></div></a>
+            <a href="https://webshop.stenden.com/"><div class="shop"></div></a>
         </div>
     </div>    
 </div>

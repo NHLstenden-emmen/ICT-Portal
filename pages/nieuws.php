b12b4125e3100c212dc88ea97adaf12aa22b7e66
Feike Falkena <feike@feikefalkena.nl>
2020-10-20 13:25:16 +0200

    Rand bug fix
    Niet klikbare items wel klikbaar maken. Bug kwam door display in de rand css regels

--- pages/nieuws.php
+++ pages/nieuws.php
@@ -2,25 +2,15 @@
 		<div class="subTitle">
 			Uitgelicht nieuws
 		</div>
-		<div class="contentBlock-grid">
-			<div class='contentBlock' style="width: 300px; height: 300px;">
-				<div class='contentBlock-side' style="width: 700px; height: 300px;"></div>
-				<div class='contentBlock-content' style="width: 700px; height: 300px;">
-					<div class='contentBlock-title'>
-						Hallo
-					</div>
-					<div class='contentBlock-text'>
-						test
-					</div>
-					<div class='contentBlock-info'>
-						<div class='contentBlock-date'>
-							vandaag om 15:09
-						</div>
-						<div class='contentBlock-link'>
-							<a href="">Lees meer</a>
-						</div>
-					</div>
+		</div class="items">
+			<div class="item rand">		
+				<div class="titel"></div>
+				<div class="beschrijving"></div>
+				<div class="metadata">
+					<div class="tijd"></div>
+					<div class="meer"></div>
 				</div>
+
 			</div>
 		</div>
 	</main>

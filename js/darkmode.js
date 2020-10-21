89de7f84747d8ad772ea4a36f43e86d67db9cb05
kevinsmul <smulders.kevin.1999@gmail.com>
2020-10-21 23:45:13 +0200

    deleted unsued file

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,61 +1,61 @@
 // check for saved 'darkMode' in localStorage
-let darkMode = localStorage.getItem('darkMode'); 
+let darkMode = localStorage.getItem("darkMode");
 
-const darkModeToggleDestkop = document.querySelector('.darkmodeSwitch');
-const darkModeToggleMobile = document.querySelector('.darkmodeSwitchMobile');
+const darkModeToggleDestkop = document.querySelector(".darkmodeSwitch");
+const darkModeToggleMobile = document.querySelector(".darkmodeSwitchMobile");
 
 const enableDarkMode = () => {
-  // 1. Add the class to the body
-  $('body').addClass('darkmode');
-  $('input').addClass('darkmode');
-  $('.inputWithIcon').addClass('darkmode');
-  $('button').addClass('darkmode');
+	// 1. Add the class to the body
+	$("body").addClass("darkmode");
+	$("input").addClass("darkmode");
+	$(".inputWithIcon").addClass("darkmode");
+	$("button").addClass("darkmode");
 
-  // 2. Update darkMode in localStorage
-  localStorage.setItem('darkMode', 'enabled');
-}
+	// 2. Update darkMode in localStorage
+	localStorage.setItem("darkMode", "enabled");
+};
 
 const disableDarkMode = () => {
-  // 1. Remove the class from the body
-  $('body').removeClass('darkmode');
-  $('input').removeClass('darkmode');
-  $('.inputWithIcon').removeClass('darkmode');
-  $('button').removeClass('darkmode');
+	// 1. Remove the class from the body
+	$("body").removeClass("darkmode");
+	$("input").removeClass("darkmode");
+	$(".inputWithIcon").removeClass("darkmode");
+	$("button").removeClass("darkmode");
 
-  // 2. Update darkMode in localStorage 
-  localStorage.setItem('darkMode', null);
-}
- 
+	// 2. Update darkMode in localStorage
+	localStorage.setItem("darkMode", null);
+};
+
 // If the user already visited and enabled darkMode
 // start things off with it on
-if (darkMode === 'enabled') {
-  enableDarkMode();
+if (darkMode === "enabled") {
+	enableDarkMode();
 }
 
 // When someone clicks the button
-darkModeToggleDestkop.addEventListener('click', () => {
-  // get their darkMode setting
-  darkMode = localStorage.getItem('darkMode'); 
-  
-  // if it not current enabled, enable it
-  if (darkMode !== 'enabled') {
-    enableDarkMode();
-  // if it has been enabled, turn it off  
-  } else {  
-    disableDarkMode(); 
-  }
+darkModeToggleDestkop.addEventListener("click", () => {
+	// get their darkMode setting
+	darkMode = localStorage.getItem("darkMode");
+
+	// if it not current enabled, enable it
+	if (darkMode !== "enabled") {
+		enableDarkMode();
+		// if it has been enabled, turn it off
+	} else {
+		disableDarkMode();
+	}
 });
 
 // When someone clicks the button
-darkModeToggleMobile.addEventListener('click', () => {
-  // get their darkMode setting
-  darkMode = localStorage.getItem('darkMode'); 
-  
-  // if it not current enabled, enable it
-  if (darkMode !== 'enabled') {
-    enableDarkMode();
-  // if it has been enabled, turn it off  
-  } else {  
-    disableDarkMode(); 
-  }
+darkModeToggleMobile.addEventListener("click", () => {
+	// get their darkMode setting
+	darkMode = localStorage.getItem("darkMode");
+
+	// if it not current enabled, enable it
+	if (darkMode !== "enabled") {
+		enableDarkMode();
+		// if it has been enabled, turn it off
+	} else {
+		disableDarkMode();
+	}
 });

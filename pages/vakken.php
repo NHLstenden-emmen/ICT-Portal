da2570b62abcd1bc3f30448240010bc3df50db15
Feike Falkena <feike@feikefalkena.nl>
2020-10-21 10:56:33 +0200

    Update vakken.php
    Nieuwe vakken pagina
    
    Co-Authored-By: rhassing1998 <56292460+rhassing1998@users.noreply.github.com>

--- pages/vakken.php
+++ pages/vakken.php
@@ -1,109 +1,44 @@
-<?php
-        $docentenData = array(
-                '1' => array(
-                    'docent_id' => 3,
-                    'voornaam' => 'Gerjan',
-                    'achternaam' => 'van Oenen',
-                    'email' => 'erat.volutpat.Nulla@eleifend.org',
-                    'telefoonnummer' => '1',
-                    'gebruikersnaam' => 'JinWeaver',
-                    'wachtwoord' => 'indigo',
-                    'foto' => 'Gerjan van Oenen.jpg',
-                    'twitter' => '',
-                    'linkedin' => '',
-                    'instagram' => ''),
-                '2' => array(
-                    'docent_id' => 4,
-                    'voornaam' => 'Tanek',
-                    'achternaam' => 'Fischer',
-                    'email' => 'Suspendisse@tempus.net',
-                    'telefoonnummer' => '1',
-                    'gebruikersnaam' => 'ThaddeusMartinez',
-                    'wachtwoord' => 'violet',
-                    'foto' => '',
-                    'twitter' => '',
-                    'linkedin' => '',
-                    'instagram' => ''),
-                '3' => array(
-                    'docent_id' => 5,
-                    'voornaam' => 'Justin',
-                    'achternaam' => 'Benton',
-                    'email' => 'luctus.ipsum.leo@Etiamvestibulummassa.co.uk',
-                    'telefoonnummer' => '1',
-                    'gebruikersnaam' => 'NoahBullock',
-                    'wachtwoord' => 'orange',
-                    'foto' => '',
-                    'twitter' => '',
-                    'linkedin' => '',
-                    'instagram' => ''),
-                '4' => array(
-                    'docent_id' => 6,
-                    'voornaam' => 'Bruno',
-                    'achternaam' => 'Barrera',
-                    'email' => 'ante.blandit.viverra@Quisqueliberolacus.edu',
-                    'telefoonnummer' => '1',
-                    'gebruikersnaam' => 'RahimBolton',
-                    'wachtwoord' => 'orange',
-                    'foto' => '',
-                    'twitter' => '',
-                    'linkedin' => '',
-                    'instagram' => ''),
-                '5' => array(
-                    'docent_id' => 7,
-                    'voornaam' => 'Robert',
-                    'achternaam' => 'Solis',
-                    'email' => 'Mauris@Vestibulumanteipsum.co.uk',
-                    'telefoonnummer' => '1',
-                    'gebruikersnaam' => 'AmeryWagner',
-                    'wachtwoord' => 'blue',
-                    'foto' => '',
-                    'twitter' => '',
-                    'linkedin' => '',
-                    'instagram' => ''),
-                '6' => array(
-                    'docent_id' => 8,
-                    'voornaam' => 'Linus',
-                    'achternaam' => 'Delacruz',
-                    'email' => 'a.feugiat@aliquet.org',
-                    'telefoonnummer' => '1',
-                    'gebruikersnaam' => 'OrlandoOsborn',
-                    'wachtwoord' => 'yellow',
-                    'foto' => '',
-                    'twitter' => '',
-                    'linkedin' => '',
-                    'instagram' => ''),
-                '7' => array(
-                    'docent_id' => 9,
-                    'voornaam' => 'Joseph',
-                    'achternaam' => 'Patterson',
-                    'email' => 'tempor.arcu.Vestibulum@erat.net',
-                    'telefoonnummer' => '1',
-                    'gebruikersnaam' => 'SolomonHenderson',
-                    'wachtwoord' => 'green',
-                    'foto' => '',
-                    'twitter' => '',
-                    'linkedin' => '',
-                    'instagram' => ''));
-            ?>
-
-
-
 <main class="content">
-    <div class="subTitle">Docentenlijst</div>
-		<div class = "docentenvakken">
-			<?php
-			foreach ($docentenData as $docent) {
-				echo "
-				<div class = 'docentenvak' id= '$docent[docent_id]'>
-					<div class = 'green'>
-					</div>
-					<div class = 'white'>
-						<img class='docentenfoto' src='Images/$docent[foto]' alt='Foto van $docent[voornaam]'>
-						<p><b>$docent[voornaam] $docent[achternaam] </b></p>
-						<p>$docent[wachtwoord]</p>
-					</div>
-				</div>";
-			}
-			?>
-		</div>
+    <div class="left_content"><br />
+
+    <?php 
+
+    if(isset($_POST['submitVakken'])){
+        //Query voor alle vakken uit dat jaar.
+
+        echo "<div class='subTitle'>Jaar {$_POST['jaarSelectie']} | Vakken</div>
+        <div class = 'contentBlock-grid'>";
+
+
+        $result = $DB->Get("SELECT * FROM docenten");
+        
+        while($docentData = $result->fetch_assoc()){
+        
+        $docentenLink = 'window.location.href="/vak?vak='.$docentData['docent_id'].'"';
+        echo "<div class='contentBlock' onclick='{$docentenLink}'>
+            <div class='contentBlock-side'></div>
+            <div class='contentBlock-content'>
+            <div class='contentBlock-title'>Vaknaam</div>
+            <div class='contentBlock-text'>Vakbeschrijving</div>
+            </div>
+            </div>";
+        }
+        echo "</div>";
+    }
+    else {
+?>
+        <p>Selecteer hier een jaar om alle vakken van het betreffende jaar te tonen.</p>
+        <form method="POST">
+        <select name="jaarSelectie">
+            <option value="1">Jaar 1</option>
+            <option value="2">Jaar 2</option>
+            <option value="3">Jaar 3</option>
+            <option value="4">Jaar 4</option>
+        </select>
+        <button type="submit" name="submitVakken">opslaan</button>
+        </form>
+<?php      
+    }
+    ?>
+    </div>
 </main>

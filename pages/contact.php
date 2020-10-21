5a4dd9e1e2defba2040b4fe61f0d6b837c356744
Nick Wind <32290253+nick90w@users.noreply.github.com>
2020-10-21 10:33:18 +0200

    added contact, mail handling, css contact
    contact toegevoegd

--- pages/contact.php
+++ pages/contact.php
@@ -1,18 +1,27 @@
-
-<main class="content">
-  <div class="subTitle">Test</div>
-		<div class = "contentBlock-grid">
-      <div class='contentBlock' style="width: 300px; height: 300px;">
-        <div class='contentBlock-side' style="width: 700px; height: 300px;"></div>
-          <div class='contentBlock-content' style="width: 700px; height: 300px;">
-            <div class='contentBlock-title'>Hallo</div>
-            <div class='contentBlock-text'>test</div>
-            <div class='contentBlock-info'>
-              <div class='contentBlock-date'>vandaag om 15:09</div>
-              <div class='contentBlock-link'><a href="">Lees meer</a></div>
-          </div>
-          </div>
-        </div>
-    </div>
-  </div>
-</main>
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="../css/contact.css">
+	<div class="contact">
+    <div class="sidebar"></div>
+        <div class="formulier">
+            <h1>Contact</h1><br>
+            <p>Neem contact op door middel van dit formulier.</p><br>
+            <h2>Contactformulier:</h2><br>
+            <form class="myForm" id="contactForm" action="mail.php" method="post">
+                <input type="text" id="fname" name="fname" placeholder="First Name"/><br>
+                <input type="text" id="email" name="email" placeholder="Email"/><br>
+                <textarea id="subject" name="message" placeholder="Place your message here"
+                          rows="4" cols="50"></textarea><br>
+                <input type="submit" id="submit" name="submit" value="Submit"/>
+            </form>
+            </div>
+            <div class="contactInfo">
+                    Telefoonnummer: 0612312312<br>
+                    Adresgegevens: Van schaikweg 26<br>
+                    Emailadres: secretariaat@deschool.nl<br>
+            </div>
+            <div class="kaartSchool">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2413.4466515339077!2d6.910182015960468!3d52.77825362481008!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b7e61f3ec72443%3A0xbe9d297b3e4fbcc7!2sNHL%20Stenden%20Hogeschool!5e0!3m2!1snl!2snl!4v1603038721287!5m2!1snl!2snl" width="300" height="225" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+            </div>
+	</div>

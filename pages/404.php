8dd53ebb20ee31fb82df07c4b7f5c5b12b00b877
DaisyBruggeman <71065775+DaisyBruggeman@users.noreply.github.com>
2020-10-26 14:01:43 +0100

    404 aanpassing
    Image groter gemaakt

--- pages/404.php
+++ pages/404.php
@@ -2,5 +2,5 @@
 <link rel="stylesheet" type="text/css" href="404.css">
 <main class="content">
     <h1>404</h1>
-    <img src="../project-periode-1/images/404.jpg" alt="mooie 404 meme">
+    <img src="../project-periode-1/images/404.jpg" alt="mooie 404 meme" id="error">
 </main>
